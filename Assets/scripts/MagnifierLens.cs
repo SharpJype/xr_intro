b95c3fc84b645bc3b783db54dc5c669d7b709741
SharpJype <jyjool10@gmail.com>
2026-02-08 20:54:37 +0200

    homework 2 progress

---
+++ Assets/scripts/MagnifierLens.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class MagnifierLens : MonoBehaviour
+{
+    public Camera lensCamera;
+    public Camera playerCamera;
+
+    private Vector3 pos1;
+    private Vector3 pos2;
+    private Quaternion rot;
+    private bool active = true;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            UpdateLensCamera();
+        }
+        
+    }
+
+    void UpdateLensCamera()
+    {
+        //Quaternion rot = new Quaternion();
+        transform.GetPositionAndRotation(out pos2, out rot);
+        playerCamera.transform.GetPositionAndRotation(out pos1, out rot);
+
+        Vector3 posDiff = pos2-pos1;
+        rot.SetLookRotation(posDiff);
+
+        lensCamera.transform.SetPositionAndRotation(pos2+posDiff, rot);
+        //transform.SetPositionAndRotation(pos2, rot);
+    }
+}
