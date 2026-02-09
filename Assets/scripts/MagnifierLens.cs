0f3196a97c605c0c35e4733036cf929730c6adcd
SharpJype <jyjool10@gmail.com>
2026-02-09 21:59:05 +0200

    homework 2 progress

--- Assets/scripts/MagnifierLens.cs
+++ Assets/scripts/MagnifierLens.cs
@@ -28,11 +28,12 @@ public class MagnifierLens : MonoBehaviour
     {
         //Quaternion rot = new Quaternion();
         transform.GetPositionAndRotation(out pos2, out rot);
+        float z = rot.eulerAngles.z;
         playerCamera.transform.GetPositionAndRotation(out pos1, out rot);
 
         Vector3 posDiff = pos2-pos1;
         rot.SetLookRotation(posDiff);
-        rot.eulerAngles = new Vector3(rot.eulerAngles.x, rot.eulerAngles.y, -rot.eulerAngles.z);
+        rot.eulerAngles = new Vector3(rot.eulerAngles.x, rot.eulerAngles.y, -rot.eulerAngles.z+z);
 
         lensCamera.transform.SetPositionAndRotation(pos2+posDiff, rot);
         //transform.SetPositionAndRotation(pos2, rot);
